dbeb62aaf70e1d1d4a80bfad496a59412a060271
Paul <pauldro952@outlook.com>
2021-07-16 09:03:35 -0500

    Add route for /inv/validate/warehouse/bin/

--- site/templates/ajax-json.php
+++ site/templates/ajax-json.php
@@ -12,6 +12,7 @@
 				['GET', 'itemid/', Json\Min::class, 'validateItemid'],
 				['GET', 'warehouse/', Json\Min::class, 'validateWarehouseid'],
 				['GET', 'warehouse/bins/', Json\Min::class, 'validateWarehouseBinid'],
+				['GET', 'warehouse/bin/', Json\Min::class, 'validateWarehouseBinid'],
 				['GET', 'itmp/exists/', Json\Min::class, 'validateItmpExists'],
 				['GET', 'item-group/', Json\Min::class, 'validateInvGroupCode'],
 				'upcx' => [
