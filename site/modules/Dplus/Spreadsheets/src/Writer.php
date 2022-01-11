f1947bc03864d924b81f960917c795a415d69985
Paul <pauldro952@outlook.com>
2022-01-11 12:00:59 -0600

    Create Dplus\Spreadsheets module for writing spreadsheets

--- site/modules/Dplus/Spreadsheets/src/Writer.php
+++ site/modules/Dplus/Spreadsheets/src/Writer.php
@@ -2,7 +2,20 @@
 // PhpSpreadsheet Library
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx as WriterXlsx;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
+// ProcessWire
+use ProcessWire\WireData;
 
+/**
+ * Writer
+ * Base Class for Writing Spreadsheets
+ *
+ * @property string $directory  Directory to Write file to
+ * @property string $filename   File Name
+ * @property string $fileprefix Prefix to Filename
+ */
 abstract class Writer extends WireData {
 	const EXTENSION = 'xlsx';
 
@@ -35,5 +48,37 @@ abstract class Writer extends WireData {
 	public function write(Spreadsheet $spreadsheet) {
 		$writer = $this->getWriter($spreadsheet);
 		return $writer->save($this->getFilepath());
+	}
+
+
+	/**
+	 * Returns Spreadsheet Alignment Code
+	 * @param  string $justify  Code given e.g. r, right
+	 * @return string
+	 */
+	public static function getAlignmentCode($justify) {
+		switch (substr($justify, 0, 1)) {
+			case 'r':
+				return Alignment::HORIZONTAL_RIGHT;
+				break;
+			case 'c':
+				return Alignment::HORIZONTAL_CENTER;
+				break;
+			default:
+				return Alignment::HORIZONTAL_LEFT;
+				break;
+		}
+	}
+
+	/**
+	 * Set Columns to be autowidth
+	 * @param Worksheet $sheet       Sheet
+	 * @param int       $columncount Number of Columns to iterate
+	 */
+	public static function setColumnsAutowidth(Worksheet $sheet, int $columncount) {
+		for ($i = 0; $i < ($columncount); $i++) {
+			$index = Coordinate::stringFromColumnIndex($i);
+			$sheet->getColumnDimension($index)->setAutoSize(true);
+		}
 	}
 }
